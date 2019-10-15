0b8eacdd843b8530af65d8a084869aed67f26836
Hossam Hassan <hossamhafez12@users.campus>
2019-10-14 22:39:44 -0500

    Point Milestone1

--- PointOfSale/DrinkSelection.xaml.cs
+++ PointOfSale/DrinkSelection.xaml.cs
@@ -24,5 +24,9 @@ namespace PointOfSale
         {
             InitializeComponent();
         }
+        void SelectFlavor(object sender, RoutedEventArgs args)
+        {
+            NavigationService.Navigate(new FlavorSelection());
+        }
     }
 }
