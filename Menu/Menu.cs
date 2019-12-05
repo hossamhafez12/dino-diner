913510e5a951fed73378eb79d486863e0742f7a3
hossamhafez12 <hossamhafez12@F0064-06-2330>
2019-12-04 18:18:44 -0600

    Try.

--- Menu/Menu.cs
+++ Menu/Menu.cs
@@ -95,6 +95,42 @@ namespace DinoDiner.Menu
                 return availableDrinks;
             }
         }
+        public List<string> possibleIngridents
+        {
+            get
+            {
+                List<string> possible = new List<string>();
+                {
+                    possible.Add("Bun");
+                    possible.Add("Peppers");
+                    possible.Add("Onions");
+                    possible.Add("Chicken Nugget");
+                    possible.Add("Peanut Butter");
+                    possible.Add("Jelly");
+                    possible.Add("Chicken");
+                    possible.Add("Pickle");
+                    possible.Add("Ketchup");
+                    possible.Add("Mustard");
+                    possible.Add("Tomato");
+                    possible.Add("Mayo");
+                    possible.Add("Lettuce");
+                    possible.Add("Tortilla");
+                    possible.Add("Chicken Breast");
+                    possible.Add("Ceasar Dressing");
+                    possible.Add("Parmesan Cheese");
+                    possible.Add("Steakburger Pattie");
+                    possible.Add("Wing Sauce");
+                    possible.Add("Potato");
+                    possible.Add("Salt");
+                    possible.Add("Vegetable oil");
+                    possible.Add("Macroni Noodles");
+                    possible.Add("Cheese Product");
+                    possible.Add("Pork Sausage");
+                    possible.Add("Breading");
+                }
+                return possible;
+            }
+        }
         /// <summary>
         /// This is a available combos property with just a getter
         /// </summary>
@@ -132,6 +168,66 @@ namespace DinoDiner.Menu
             }
             return st.ToString();
         }
+        public List<IMenuItem> Search(List<IMenuItem> items, string searchString)
+        {
+            List<IMenuItem> res = new List<IMenuItem>();
 
+            foreach (IMenuItem entr in AvailableMenuItems)
+            {
+                if (entr.ToString().Contains(searchString))
+                {
+                    res.Add(entr);
+                }
+            }
+            return res;
+        }
+        public List<IMenuItem> ApplyFilter(List<IMenuItem> searchRes, List<string> menuCatogri)
+        {
+            List<IMenuItem> result = new List<IMenuItem>();
+            foreach (IMenuItem item in searchRes)
+            {
+                if (item is Entree && menuCatogri.Contains("Entree"))
+                {
+                    result.Add(item);
+                }
+                else if (item is CretaceousCombo && menuCatogri.Contains("Combo"))
+                {
+                    result.Add(item);
+                }
+                else if (item is Side && menuCatogri.Contains("Side"))
+                {
+                    result.Add(item);
+                }
+                else if (item is Drink && menuCatogri.Contains("Drink"))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+        public List<IMenuItem> FilterByMinPrice(List<IMenuItem> movies, float minPrice)
+        {
+            List<IMenuItem> results = new List<IMenuItem>();
+            foreach (IMenuItem items in AvailableMenuItems)
+            {
+                if (items.Price != 0 && minPrice >= items.Price)
+                {
+                    results.Add(items);
+                }
+            }
+            return results;
+        }
+        public List<IMenuItem> FilterByMaxPrice(List<IMenuItem> movies, float maxPrice)
+        {
+            List<IMenuItem> results = new List<IMenuItem>();
+            foreach (IMenuItem items in AvailableMenuItems)
+            {
+                if (items.Price != 0 && maxPrice <= items.Price)
+                {
+                    results.Add(items);
+                }
+            }
+            return results;
+        }
     }
 }
--- Website/Pages/Menu.cshtml.cs
+++ Website/Pages/Menu.cshtml.cs
@@ -11,8 +11,39 @@ namespace Website.Pages
     public class MenuModel : PageModel
     {
         public Menu Menu { get; } = new Menu();
+        [BindProperty]
+        public string search { get; set; }
+        [BindProperty]
+        public List<string> menuCategory { get; set; } = new List<string>();
+        public List<IMenuItem> Items { get; set; }
+        [BindProperty]
+        public float? minPrice { get; set; }
+        [BindProperty]
+        public float? maxPrice { get; set; }
         public void OnGet()
         {
+            Items = Menu.AvailableMenuItems;
+        }
+        public void OnPost()
+        {
+            Items = Menu.AvailableMenuItems;
+
+            if (search != null)
+            {
+                Items = Menu.Search(Items, search);
+            }
+            if (menuCategory.Count != 0)
+            {
+                Items = Menu.ApplyFilter(Items, menuCategory);
+            }
+            if (minPrice != null)
+            {
+                Items = Menu.FilterByMinPrice(Items, (float)minPrice);
+            }
+            if (maxPrice != null)
+            {
+                Items = Menu.FilterByMaxPrice(Items, (float)maxPrice);
+            }
 
         }
     }
